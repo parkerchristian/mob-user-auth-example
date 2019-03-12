8584300ef006034509926312bfe14cd73d8ff77d
Parker Helland <hellandp@gmail.com>
2019-03-12 14:28:45 -0700

    made loadQuery function then called it inside evenListener

--- src/index.js
+++ src/index.js
@@ -3,16 +3,28 @@ import loadMovies from './movie-list-component.js';
 import './search-component.js';
 import { readFromQuery } from './hash-query-component.js';
 import { makeApiURL } from './make-api-url-component.js';
+import { auth } from './firebase.js';
 
 loadHeader();
 
-window.addEventListener('hashchange', () => {
+function loadQuery() {
     const existingQuery = window.location.hash.slice(1);
     const searchOptions = readFromQuery(existingQuery);
     const url = makeApiURL(searchOptions);
+    
+    if(!existingQuery) {
+        return;
+    }
+
     fetch(url)
         .then(response => response.json())
         .then(body => {
             loadMovies(body.results);
         });
+}
+
+window.addEventListener('hashchange', loadQuery);
+
+auth.onAuthStateChanged(() => {
+    loadQuery();
 });
