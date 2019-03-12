52fb1e6421f08c7ef718fb60a8def3e5bf14f770
Parker Helland <hellandp@gmail.com>
2019-03-12 13:30:19 -0700

    setup database

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,14 @@
 var config = {
     apiKey: 'AIzaSyAoOoJD9ZvwxaAFENSYn4F7DGlAg8HX6cc',
     authDomain: 'mobbookrepo.firebaseapp.com',
+    databaseURL: 'https://mobbookrepo.firebaseio.com',
+    projectId: 'mobbookrepo'
 };
   
 firebase.initializeApp(config);
 
+
 export const auth = firebase.auth();
+export const db = firebase.database();
+export const userRef = db.ref('user');
+export const favoritesByUserRef = db.ref('favorites-by-user');
