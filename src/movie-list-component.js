fe139f770fb0bcdfd5e4e521187c023a5ba4ddff
Parker Helland <hellandp@gmail.com>
2019-03-11 17:00:06 -0700

    fix img src path on list results

--- src/movie-list-component.js
+++ src/movie-list-component.js
@@ -1,8 +1,9 @@
 export function makeMovieTemplate(movie) {
+    const BASE_IMG_URL = `https://image.tmdb.org/t/p/w154${movie.poster_path}`;
     const html = `
         <li>
             <h2>${movie.title}</h2>
-            <img src="${movie.src}">
+            <img src="${BASE_IMG_URL}">
         </li>
     `;
     const template = document.createElement('template');
